42feffdd51663c2580e1d8e55c0a915f1bd8259c
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-10-15 13:47:47 +0100

    Draw mountain

--- mountain.py
+++ mountain.py
@@ -3,7 +3,7 @@ from turtle import *
 speed(0)
 bgcolor("skyblue")
 
-## Grass
+# Grass
 penup()
 goto(-400, -100)
 pendown()
@@ -15,3 +15,27 @@ for i in range(2):
     forward(400)
     right(90)
 end_fill()
+
+# Left mountain
+penup()
+goto(-400, -100)
+pendown()
+color("dimgray")
+begin_fill()
+for i in range(3):
+     forward(300)
+     left (120)
+end_fill()
+
+# Right mountain
+penup()
+goto(100, -100)
+pendown()
+begin_fill()
+for i in range(3):
+     forward(300)
+     left (120)
+end_fill()
+
+
+
